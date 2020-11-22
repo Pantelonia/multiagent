94548e8a395cee6e2658b74203e6d97cbaee8dc7
Paul <pavel1107syr@gmail.com>
2020-11-22 23:02:34 +0100

    Finish 5 task. Aded 3 and 4 task by teameit

--- multiAgents.py
+++ multiAgents.py
@@ -14,7 +14,7 @@
 
 from util import manhattanDistance
 from game import Directions
-import random, util
+import random, util, sys
 
 from game import Agent
 
@@ -195,8 +195,53 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
           Returns the minimax action using self.depth and self.evaluationFunction
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        "* YOUR CODE HERE *"
+        inf = float('inf')
+        action, score = self.alpha_beta(0, 0, gameState, -inf, inf)  # Get the action and score with the alpha-beta pruning
+        return action  
+
+    def alpha_beta(self, depth, index, gameState, alpha, beta):
+      
+      # Returns the best score by using the alpha beta algorithm
+      if index >= gameState.getNumAgents():   # increase depth by one if all the agents ended their turn
+          index = 0
+          depth += 1
+
+      if depth == self.depth:                               # we return evaluation function if we have reached the maximum depth
+          return None, self.evaluationFunction(gameState)
+
+      best_score, best_action = None, None
+
+      if index == 0:  
+          for action in gameState.getLegalActions(index):                   # we get the minimax score of the successor for each possible action of the pacman
+              following_state = gameState.generateSuccessor(index, action)
+              _, score = self.alpha_beta(depth, index + 1, following_state, alpha, beta)
+
+              if best_score is None or score > best_score:
+                  best_score = score
+                  best_action = action
+              alpha = max(alpha, score)     # we reupdate the variable alpha with the maximum value
+
+              if alpha > beta:    # we prune the tree using break if alpha is bigger than beta
+                  break
+
+      else: 
+          for action in gameState.getLegalActions(index):                   # we get the minimax score of the successor for each possible action of the ghost
+              following_state = gameState.generateSuccessor(index, action)
+              _, score = self.alpha_beta(depth, index + 1, following_state, alpha, beta)
+
+              if best_score is None or score < best_score:
+                best_score = score
+                best_action = action
+              beta = min(beta, score)     # we reupdate the variable alpha with the maximum value
+
+              if beta < alpha:       # we prune the tree using break if beta is smaller than alpha
+                  break
+
+      if best_score is None:                              # if our state has not successor states we return the evaluation function
+        return None, self.evaluationFunction(gameState)
+
+      return best_action, best_score                      # otherwise we return the best_action and best_score
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -210,19 +255,134 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
           All ghosts should be modeled as choosing uniformly at random from their
           legal moves.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        "* YOUR CODE HERE *"
+
+        action, score = self.expectimax(0, 0, gameState)  # Get the action and score for pacman
+        return action  
+
+    def expectimax(self, depth, index, gameState):
+        '''
+        Returns the best score for an agent using the expectimax algorithm. For max player (agent_index=0), the best
+        score is the maximum score among its successor states and for the min player (agent_index!=0), the best
+        score is the average of all its successor states. Recursion ends if there are no successor states
+        available or depth equals the max depth to be searched until.
+        :param depth: the current depth of the tree (int)
+        :param agent_index: index of the current agent (int)
+        :param gameState: the current state of the game (GameState)
+        :return: action, score
+        '''
+
+        # Returns the best score by using the expectimax algorithm
+
+        if index >= gameState.getNumAgents():   # increase depth by one if all the agents ended their turn
+            index = 0
+            depth += 1
+
+        if depth == self.depth:                               # we return evaluation function if we have reached the maximum depth
+            return None, self.evaluationFunction(gameState)
+
+        best_score, best_action = None, None
+
+        if index == 0:  
+            for action in gameState.getLegalActions(index):  # we get the minimax score of the successor for each possible action of the pacman
+                next_game_state = gameState.generateSuccessor(index, action)
+                _, score = self.expectimax(depth, index + 1, next_game_state)
+
+                if best_score is None or score > best_score:
+                    best_score = score
+                    best_action = action
+
+        else:  # If it is min player's (ghost) turn
+            ghostActions = gameState.getLegalActions(index)     # we get the expectmax score of the successor for each possible action of the ghost
+            if len(ghostActions) is not 0:
+                prob = 1.0 / len(ghostActions)
+            for action in gameState.getLegalActions(index): 
+                following_state = gameState.generateSuccessor(index, action)
+                _, score = self.expectimax(depth, index + 1, following_state)
+
+                if best_score is None:
+                    best_score = 0.0
+                best_score += prob * score
+                best_action = action
+
+        if best_score is None:                                  # if our state has not successor states we return the evaluation function
+            return None, self.evaluationFunction(gameState)
+
+        return best_action, best_score        # otherwise we return the best_action and best_score
 
 def betterEvaluationFunction(currentGameState):
-    """
-      Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
-      evaluation function (question 5).
-
-      DESCRIPTION: <write something here so we know what you did>
-    """
+   
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # Useful information you can extract from a GameState (pacman.py)
 
+    # Initial all varieble that we needed
+    if currentGameState.isWin() :  return sys.maxint
+    if currentGameState.isLose() :  return -sys.maxint
+    
+    currentPosition = currentGameState.getPacmanPosition()
+    currentFood = currentGameState.getFood()
+    capsulePosition = currentGameState.getCapsules()
+    GhostStates = currentGameState.getGhostStates()
+
+    ratioFood, ratioGhost, ratioCapsule, ratioHunter = 5.0, 5.0, 5.0, 0.0
+    ghostScore, capsuleScore, hunterScore = 0.0, 0.0, 0.0
+    
+    #Found a closest food  
+    currentFoodList = currentFood.asList()
+    closestFood = min([util.manhattanDistance(currentPosition, foodPos) for foodPos in currentFoodList])
+    foodScore = 1.0 / closestFood
+    
+    #Found others scope
+    if GhostStates:
+        #take information about ghost
+        ghostPositions = [ghostState.getPosition() for ghostState in GhostStates]
+        ghostDistances = [util.manhattanDistance(currentPosition, ghostPos) for ghostPos in ghostPositions]
+        ScaredTimes = [ghostState.scaredTimer for ghostState in GhostStates]
+        #not hunting mode
+        if sum(ScaredTimes) == 0 : 
+            closestGhost = min(ghostDistances)
+            ghostCenterPos = ( sum([ghostPos[0] for ghostPos in ghostPositions])/len(GhostStates),\
+                               sum([ghostPos[1] for ghostPos in ghostPositions])/len(GhostStates))
+            ghostCenterDist = util.manhattanDistance(currentPosition, ghostCenterPos)
+            if ghostCenterDist <= closestGhost and closestGhost >= 1 and closestGhost <= 5:
+                if len(capsulePosition) != 0:
+                    closestCapsule = min([util.manhattanDistance(capsule,currentPosition) for capsule in capsulePosition])
+                    if closestCapsule <= 3:
+                        ratioCapsule, capsuleScore = 20.0, (1.0 / closestCapsule)
+                        ratioGhost, ghostScore = 3.0, (-1.0 / (ghostCenterDist+1))
+                    else:
+                        ratioGhost, ghostScore = 10.0, (-1.0 / (ghostCenterDist+1))
+                else:
+                    ratioGhost, ghostScore = 10.0, (-1.0 / (ghostCenterDist+1))
+            else:
+                ghostScore = -1.0 / closestGhost
+        else: # hunter mode
+            normalGhostDist = []
+            closestPrey = sys.maxint
+            ghostCenterX, ghostCenterY = 0.0, 0.0
+            for (index, ghostDist) in enumerate(ghostDistances):
+                if ScaredTimes[index] == 0 :
+                    normalGhostDist.append(ghostDist)
+                    ghostCenterX += ghostPositions[index][0]
+                    ghostCenterY += ghostPositions[index][1]
+                else:
+                    if ghostDist <= ScaredTimes[index] :
+                        if ghostDist < closestPrey:
+                            closestPrey = ghostDistances[index]
+            if normalGhostDist:
+                closestGhost = min(normalGhostDist)
+                ghostCenterPos = ( ghostCenterX/len(normalGhostDist), ghostCenterY/len(normalGhostDist))
+                ghostCenterDist = util.manhattanDistance(currentPosition, ghostCenterPos)
+                if ghostCenterDist <= closestGhost and closestGhost >= 1 and closestGhost <= 5:
+                    ratioGhost, ghostScore = 10.0, (- 1.0 / (ghostCenterDist+1))
+                else:
+                    ghostScore = - 1.0 / closestGhost
+            ratioHunter, hunterScore = 35.0, (1.0 / closestPrey)
+    
+    # Take a result
+    heuristic = currentGameState.getScore() + \
+                ratioFood*foodScore + ratioGhost*ghostScore + \
+                ratioCapsule*capsuleScore + ratioHunter*hunterScore
+    return heuristic
 # Abbreviation
 better = betterEvaluationFunction
-
